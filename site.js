f88b32edd20196fc89ea922a6d8f90531dee75ce
nicholsonja <nicholsonja@apsu.edu>
2023-06-22 16:33:13 -0500

    test ajax

--- site.js
+++ site.js
@@ -3,3 +3,16 @@ function sayHi() {
     document.getElementById('output').innerText = message;
     
 }
+
+function loadQuote() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        document.getElementById('output2').innerText = this.responseText;
+      }
+    xhttp.onerror = function() {
+        document.getElementById('output2').innerText = "Problem"
+    }
+    xhttp.open("GET", "https://ron-swanson-quotes.herokuapp.com/v2/quotes", true);
+    xhttp.send();
+    
+  }
